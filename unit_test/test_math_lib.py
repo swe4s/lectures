d6852be45cdce96811943b2f334ac9d18b16b736
Ryan Layer <ryan.layer@gmail.com>
2019-09-09 10:10:05 -0600

    switch away from context manager

--- unit_test/test_math_lib.py
+++ unit_test/test_math_lib.py
@@ -19,13 +19,12 @@ class TestMathLib(unittest.TestCase):
         self.assertEqual(math_lib.prod(-1, -1), 1)
 
     def test_div(self):
-        self.assertEqual(math_lib.div(10, 5), 2)
+        self.assertEqual(math_lib.div(10, 5), 1)
         self.assertEqual(math_lib.div(-1, 1), -1)
         self.assertEqual(math_lib.div(-1, -1), 1)
         self.assertEqual(math_lib.div(5, 2), 2.5)
 
-        with self.assertRaises(ValueError):
-            math_lib.div(10, 0)
+        self.assertRaises(ZeroDivisionError, math_lib.div, 10, 0)
 
 if __name__ == '__main__':
     unittest.main()
