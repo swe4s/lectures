73d8c4e43d7e542031b2138d5d8ef6693aa9071b
Ryan Layer <ryan.layer@gmail.com>
2019-09-09 13:18:49 -0600

    add class setup and teardown

--- unit_test/test_math_lib.py
+++ unit_test/test_math_lib.py
@@ -1,5 +1,7 @@
 import unittest
 import math_lib
+import random
+import os
 
 class TestMathLib(unittest.TestCase):
 
@@ -30,9 +32,6 @@ class TestMathLib(unittest.TestCase):
             math_lib.div(10, 0)
 
     def test_file_add_no_setup(self):
-
-        import random
-
         test_file_name = 'test_file.txt'
         f = open(test_file_name, 'w')
 
@@ -48,14 +47,10 @@ class TestMathLib(unittest.TestCase):
         
         self.assertEqual(file_sum, direct_sum)
 
-        import os
-
         os.remove(test_file_name)
 
 
     def test_file_prod_no_setup(self):
-        import random
-
         test_file_name = 'test_file.txt'
         f = open(test_file_name, 'w')
 
@@ -71,10 +66,10 @@ class TestMathLib(unittest.TestCase):
         
         self.assertEqual(file_prod, direct_prod)
 
+        os.remove(test_file_name)
+
 
     def setUp(self):
-        import random
-
         self.test_file_name = 'setup_test_file.txt'
         f = open(self.test_file_name, 'w')
 
@@ -89,17 +84,42 @@ class TestMathLib(unittest.TestCase):
         f.close()
 
     def tearDown(self):
-        import os
         os.remove(self.test_file_name)
 
     def test_file_add_setup(self):
         file_sum = math_lib.file_add(self.test_file_name)
-        
         self.assertEqual(file_sum, self.direct_sum)
 
     def test_file_prod_setup(self):
         file_prod = math_lib.file_prod(self.test_file_name)
         self.assertEqual(file_prod, self.direct_prod)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.class_test_file_name = 'class_setup_test_file.txt'
+        f = open(cls.class_test_file_name, 'w')
+
+        cls.class_direct_sum = 0
+        cls.class_direct_prod = 1
+
+        for i in range(100):
+            rand_int = random.randint(1,100)
+            cls.class_direct_prod *= rand_int
+            cls.class_direct_sum += rand_int
+            f.write(str(rand_int) + '\n')
+        f.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.class_test_file_name)
+
+    def test_file_add_clas_setup(self):
+        file_sum = math_lib.file_add(self.class_test_file_name)
+        self.assertEqual(file_sum, self.class_direct_sum)
+
+    def test_file_prod_class_setup(self):
+        file_prod = math_lib.file_prod(self.class_test_file_name)
+        self.assertEqual(file_prod, self.class_direct_prod)
 
 
 if __name__ == '__main__':
