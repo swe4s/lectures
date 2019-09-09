4909dc36e0f8b5bcccebada32a1e2747eb8a0895
Ryan Layer <ryan.layer@gmail.com>
2019-09-09 12:10:12 -0600

    add examples with and without setup and teardown

--- unit_test/math_lib.py
+++ unit_test/math_lib.py
@@ -9,3 +9,19 @@ def prod(a, b):
 
 def div(a, b):
     return a / b
+
+def file_add(file_name):
+    f = open(file_name)
+    s = 0
+    for l in f:
+        s += int(l)
+    f.close()
+    return s
+
+def file_prod(file_name):
+    f = open(file_name)
+    s = 1
+    for l in f:
+        s *= int(l)
+    f.close()
+    return s
--- unit_test/test_math_lib.py
+++ unit_test/test_math_lib.py
@@ -29,5 +29,78 @@ class TestMathLib(unittest.TestCase):
         with self.assertRaises(ZeroDivisionError):
             math_lib.div(10, 0)
 
+    def test_file_add_no_setup(self):
+
+        import random
+
+        test_file_name = 'test_file.txt'
+        f = open(test_file_name, 'w')
+
+        direct_sum = 0
+
+        for i in range(100):
+            rand_int = random.randint(1,100)
+            direct_sum += rand_int
+            f.write(str(rand_int) + '\n')
+        f.close()
+
+        file_sum = math_lib.file_add(test_file_name)
+        
+        self.assertEqual(file_sum, direct_sum)
+
+        import os
+
+        os.remove(test_file_name)
+
+
+    def test_file_prod_no_setup(self):
+        import random
+
+        test_file_name = 'test_file.txt'
+        f = open(test_file_name, 'w')
+
+        direct_prod = 1
+
+        for i in range(100):
+            rand_int = random.randint(1,100)
+            direct_prod *= rand_int
+            f.write(str(rand_int) + '\n')
+        f.close()
+
+        file_prod = math_lib.file_prod(test_file_name)
+        
+        self.assertEqual(file_prod, direct_prod)
+
+
+    def setUp(self):
+        import random
+
+        self.test_file_name = 'setup_test_file.txt'
+        f = open(self.test_file_name, 'w')
+
+        self.direct_sum = 0
+        self.direct_prod = 1
+
+        for i in range(100):
+            rand_int = random.randint(1,100)
+            self.direct_prod *= rand_int
+            self.direct_sum += rand_int
+            f.write(str(rand_int) + '\n')
+        f.close()
+
+    def tearDown(self):
+        import os
+        os.remove(self.test_file_name)
+
+    def test_file_add_setup(self):
+        file_sum = math_lib.file_add(self.test_file_name)
+        
+        self.assertEqual(file_sum, self.direct_sum)
+
+    def test_file_prod_setup(self):
+        file_prod = math_lib.file_prod(self.test_file_name)
+        self.assertEqual(file_prod, self.direct_prod)
+
+
 if __name__ == '__main__':
     unittest.main()
