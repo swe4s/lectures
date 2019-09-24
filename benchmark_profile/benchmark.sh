2d38ea1c18514f7dd9e84dd8fc813b9bc057a9fd
Ryan Layer <ryan.layer@gmail.com>
2019-09-24 09:30:06 -0600

    benchmark and profile search

---
+++ benchmark_profile/benchmark.sh
@@ -0,0 +1,10 @@
+search.py 10000 1000 L
+
+search.py 10000 1000 B
+
+
+#conda install -c conda-forge time
+$HOME/miniconda3/envs/swe4s/bin/time search.py 10000 1000 L
+
+$HOME/miniconda3/envs/swe4s/bin/time search.py 10000 1000 B
+
