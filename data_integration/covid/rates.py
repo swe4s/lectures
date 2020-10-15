20aff958d7543dc2e9e51be53aa2bb3e9027a58a
Ryan Layer <ryan.layer@gmail.com>
2020-10-15 14:22:47 -0600

    add plotting

--- data_integration/covid/rates.py
+++ data_integration/covid/rates.py
@@ -1,5 +1,4 @@
 from operator import itemgetter
-import re
 from datetime import datetime
 import matplotlib
 matplotlib.use('Agg')
@@ -55,43 +54,75 @@ def get_columns(file_name,
 
     return hits
 
+def plot_lines(points, labels, file_name):
+    """Take a list of list of points and plot each list as a line.
+
+        Parameters
+        ----------
+        points    : list of list of points
+                    Each sublist corresponds to the points for one element.
+                    Each point has two values, the first will be the X value
+                    and the second the Y value
+        labels    : list of strings
+                    Each element in lables corresponds to the sublist at the
+                    same poisiting in data
+        file_name : string
+                    Name of the output file.
+    """
+    fig = plt.figure(figsize=(10,3), dpi=300)
+    ax = fig.add_subplot(1,1,1)
+
+    i = 0
+    for pairs in points:
+        X = []
+        Y = []
+        for pair in pairs:
+            X.append(pair[0])
+            Y.append(pair[1])
+
+        ax.plot(X, Y, lw=0.5)
+        ax.text(X[-1], Y[-1], labels[i], size=5)
+
+        i += 1
+
+    plt.savefig(file_name, bbox_inches='tight')
+
+
 co_county_cases = get_columns(covid_data, 2, 'Colorado', (0,1,4))
 co_county_pop = get_columns(cencus_data, 5, 'Colorado', (6,7))
 for co in co_county_pop:
-    #co[0]  = co[0][:-7]
-    co[0] = re.sub(' County', '', co[0])
+    co[0]  = co[0][:-7]
     co[1]  = int(co[1])
 
 co_county_pop.sort(key=itemgetter(0))
 
 co_rates = []
 for co in co_county_cases:
+    if co[1] == 'Unknown':
+        continue
     co_pop = binary_search(co[1], co_county_pop)
     co_rates.append((co[0], co[1], int(co[2])/co_pop))
 
 co_rates.sort(key=itemgetter(1))
 
 last_co = None
-dates = []
-rates = []
-
-fig = plt.figure(figsize=(10,3), dpi=300)
-ax = fig.add_subplot(1,1,1)
-
-
-
+curr_points = []
+points = []
+labels = []
 for date, curr_co, rate  in co_rates:
     if curr_co == last_co:
-        dates.append(datetime.strptime(date, '%Y-%m-%d'))
-        rates.append(rate)
+        curr_points.append( [datetime.strptime(date, '%Y-%m-%d'), rate ] )
     else:
-        if len(dates) > 0 and min(rates) > 0:
-            ax.plot(dates, rates, lw=0.5)
-            ax.text(dates[-1], rates[-1], last_co, size=5)
+        if len(curr_points) > 0:
+            points.append(curr_points)
+            labels.append(last_co)
 
-        dates = [datetime.strptime(date, '%Y-%m-%d')]
-        rates = [rate]
+        curr_points = [ [datetime.strptime(date, '%Y-%m-%d'), rate ] ]
 
     last_co = curr_co
 
-plt.savefig('out.png', bbox_inches='tight')
+curr_points.append( [datetime.strptime(date, '%Y-%m-%d'), rate ] )
+points.append(curr_points)
+labels.append(last_co)
+
+plot_lines(points, labels, 'test.png')
