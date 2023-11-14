e15c4daed57e248e9e5287cc5379af84fb834631
Ryan Layer <ryan.layer@gmail.com>
2023-11-14 09:01:43 -0700

    clean up

--- src/hash_tables/README.md
+++ src/hash_tables/README.md
@@ -3,13 +3,13 @@
 python hash_functions.py rand_words.txt ascii | python scatter.py ascii_hash_function.png "Hashed word" "Hashed value"
 ```
 
-![](ascii_hash_function.png)
+![](doc/ascii_hash_function.png)
 
 ```
 python hash_functions.py rand_words.txt rolling | python scatter.py rolling_hash_function_rand.png "Hashed word" "Hashed value"
 ```
 
-![](rolling_hash_function_rand.png)
+![](doc/rolling_hash_function_rand.png)
 
 ```
 for M in $( seq  1000 1000 10000 ); do
@@ -19,4 +19,4 @@ done
 grep insert rolling_linear_rand.*.txt | cut -d " " -f2,3 | python scatter.py rolling_insert_time.png "Load factor" "Insert time"
 ```
 
-![](rolling_insert_time.png)
+![](doc/rolling_insert_time.png)
--- src/hash_tables/benchmark.sh
+++ src/hash_tables/benchmark.sh
@@ -1,31 +1,35 @@
-python hash_functions.py rand_words.txt ascii | python scatter.py ascii_hash_function_rand.png "Hashed word" "Hashed value"
+python src/hash_functions.py words.txt ascii | python src/histogram.py doc/ascii_hash_function_nonrand_histogram.png "Hashed value" "Freq"
 
-python hash_functions.py rand_words.txt rolling | python scatter.py rolling_hash_function_rand.png "Hashed word" "Hashed value"
+python src/hash_functions.py words.txt rolling | python src/histogram.py doc/rolling_hash_function_nonrand_histogram.png "Hashed value" "Freq"
 
-python hash_functions.py non_rand_words.txt ascii | python scatter.py ascii_hash_function_non_rand.png "Hashed word" "Hashed value"
+python src/hash_functions.py data/rand_words.txt ascii | python src/scatter.py doc/ascii_hash_function_rand_scatter.png "Hashed word" "Hashed value"
 
-python hash_functions.py non_rand_words.txt rolling | python scatter.py rolling_hash_function_non_rand.png "Hashed word" "Hashed value"
+python src/hash_functions.py data/rand_words.txt rolling | python src/scatter.py doc/rolling_hash_function_rand_scatter.png "Hashed word" "Hashed value"
+
+python src/hash_functions.py data/non_rand_words.txt ascii | python src/scatter.py doc/ascii_hash_function_non_rand_scatter.png "Hashed word" "Hashed value"
+
+python src/hash_functions.py data/non_rand_words.txt rolling | python src/scatter.py doc/rolling_hash_function_non_rand_scatter.png "Hashed word" "Hashed value"
 
 for M in $( seq  1000 1000 10000 ); do
-    python hash_table.py 10000 ascii linear rand_words.txt $M >  ascii_linear_rand.$M.txt
+    python src/hash_table.py 10000 ascii linear data/rand_words.txt $M >  ascii_linear_rand.$M.txt
 done
 
-grep insert ascii_linear_rand.*.txt | cut -d " " -f2,3 | python scatter.py linear_insert_time.png "Load factor" "Insert time"
+grep insert ascii_linear_rand.*.txt | cut -d " " -f2,3 | python src/scatter.py doc/linear_insert_time.png "Load factor" "Insert time"
 
 (for M in $( seq  1000 1000 10000 ); do
     load_factor=$(bc -l <<< "$M/10000")
     echo -n "$load_factor " 
-    grep search ascii_linear_rand.$M.txt | cut -d " " -f2 | python mean.py
-done) | python scatter.py ascii_search_time.png "Load factor" "Search time"
+    grep search ascii_linear_rand.$M.txt | cut -d " " -f2 | python src/mean.py
+done) | python src/scatter.py doc/ascii_search_time.png "Load factor" "Search time"
 
 for M in $( seq  1000 1000 10000 ); do
-    python hash_table.py 10000 rolling linear rand_words.txt $M >  rolling_linear_rand.$M.txt
+    python src/hash_table.py 10000 rolling linear data/rand_words.txt $M >  rolling_linear_rand.$M.txt
 done
 
-grep insert rolling_linear_rand.*.txt | cut -d " " -f2,3 | python scatter.py rolling_insert_time.png "Load factor" "Insert time"
+grep insert rolling_linear_rand.*.txt | cut -d " " -f2,3 | python src/scatter.py doc/rolling_insert_time.png "Load factor" "Insert time"
 
 (for M in $( seq  1000 1000 10000 ); do
     load_factor=$(bc -l <<< "$M/10000")
     echo -n "$load_factor " 
-    grep search rolling_linear_rand.$M.txt | cut -d " " -f2 | python mean.py
-done) | python scatter.py rolling_search_time.png "Load factor" "Search time"
+    grep search rolling_linear_rand.$M.txt | cut -d " " -f2 | python src/mean.py
+done) | python src/scatter.py doc/rolling_search_time.png "Load factor" "Search time"
